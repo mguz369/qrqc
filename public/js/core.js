b622f29daf199a0eaf6bdbd9fcbf2a089f50d03e
bnetz <brian.netz@hutchinsonna.com>
2017-10-20 10:33:29 -0400

    Email changes

--- public/js/core.js
+++ public/js/core.js
@@ -1356,14 +1356,14 @@ function whichDay(dateString) {
 
 function GetToday(){
   var date = new Date()
-      var day = date.getDate();
-      var month = date.getMonth() + 1;
-      var year = date.getFullYear();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
 
-      if(day < 10)
-          day = '0' + day;
-      if(month < 10)
-          month = '0' + month;
+  if(day < 10)
+      day = '0' + day;
+  if(month < 10)
+      month = '0' + month;
   var today = year + "-" + month + "-" + day;
 
   return today;
